b7160c40b1256775500af611d284e6b450034875
diljots99@gmail.com <diljots99@gmail.com>
2022-06-09 17:52:23 +0530

    insights

--- Data-Sas-BE/app/controller/insights.js
+++ Data-Sas-BE/app/controller/insights.js
@@ -47,29 +47,34 @@ async function getInsights(req, res) {
         attributes: ["sic_section", [Sequelize.fn('COUNT', 'sic_section'), 'sectorCount'], [Sequelize.literal('(count(*)  * 100.0 / SUM(COUNT(*))  OVER() )'), "percentage"]],
         group: "sic_section"
     })
+    const companiesBySICDivision = await dao.getCompanies({
+        attributes: ["sic_division", [Sequelize.fn('COUNT', 'sic_division'), 'sectorCount'], [Sequelize.literal('(count(*)  * 100.0 / SUM(COUNT(*))  OVER() )'), "percentage"]],
+        group: "sic_division"
+    })
+    const companiesBySICCode = await dao.getCompanies({
+        attributes: ["sic_division", [Sequelize.fn('COUNT', 'sic_division'), 'sectorCount'], [Sequelize.literal('(count(*)  * 100.0 / SUM(COUNT(*))  OVER() )'), "percentage"]],
+        group: "sic_division"
+    })
 
-    const companiesByAgeOfBusiness = await dao.getCompanies({
-        attributes: ["age_of_business", [Sequelize.fn('COUNT', 'sic_section'), 'sectorCount'], [Sequelize.literal('(count(*)  * 100.0 / SUM(COUNT(*))  OVER() )'), "percentage"]],
-        group: "age_of_business"
-    })
-    const companiesByEmployeeSize = await dao.getCompanies({
-        attributes: ["size_estimate", [Sequelize.fn('COUNT', 'sic_section'), 'sectorCount'], [Sequelize.literal('(count(*)  * 100.0 / SUM(COUNT(*))  OVER() )'), "percentage"]],
-        group: "size_estimate"
-    })
-    
-    
+    const companiesByAgeOfBusiness = await dao.getCompaniesByAgeOfBusiness()
+    const companiesByEmployeeSize = await dao.getCompaniesByEmployeeSize()
+    const companiesByTurnover = await dao.getCompaniesByTurnover();
+
 
     res.send({ 
         status: true, 
         "companiesByStatus": companiesByStatus, 
         "companiesByAccountCategory": companiesByAccountCategory, 
-        "companiesBySize": [], 
         "companiesByType": companiesByType, 
         "companiesByExporter": companiesByExporter,
         "companiesByImporter": companiesByImporter,
         "companiesBySector": companiesBySector,
         "companiesBySICSection": companiesBySICSection,
-        "companiesByEmployeeSize":companiesByEmployeeSize })
+        "companiesByAgeOfBusiness":companiesByAgeOfBusiness,
+        "companiesByEmployeeSize":companiesByEmployeeSize,
+        "companiesBySICDivision":companiesBySICDivision,
+        "companiesBySICCode":companiesBySICCode,
+        "companiesByTurnover":companiesByTurnover })
 }
 
 
--- Data-Sas-BE/app/dao/insights.js
+++ Data-Sas-BE/app/dao/insights.js
@@ -1,5 +1,6 @@
 const model = require("../../models");
-
+const { Op, Sequelize } = require("sequelize");
+const { floor }  = require("mathjs")
 
 async function getCompanies(data){
     return await model.companies.findAll(data)
@@ -10,7 +11,104 @@ async function getCompaniesOfficial(data){
 
 }
 
+async function getCompaniesByEmployeeSize(data){
+    let max_size_estimate =  await model.companies.findOne({
+        attributes: [
+           
+            [Sequelize.literal('MAX(size_estimate)'), "max_size_estimate"]
+        ],
+        ...data
+    })
+    max_size_estimate = max_size_estimate? max_size_estimate.dataValues.max_size_estimate: 0
+
+    let interval = max_size_estimate / 6
+    
+    interval = floor(interval)
+    
+    return await model.companies.findAll({
+        attributes: [
+             [Sequelize.literal(`COUNT(CASE WHEN size_estimate = 0 THEN 1 END)`), "0"],
+             [Sequelize.literal(`COUNT(CASE WHEN size_estimate > 0 and size_estimate <=${interval} THEN 1 END)`), `0 - ${interval}`],
+             [Sequelize.literal(`COUNT(CASE WHEN size_estimate > ${interval} and size_estimate <=${interval*2} THEN 1 END)`), `${interval} - ${interval*2}`],
+             [Sequelize.literal(`COUNT(CASE WHEN size_estimate > ${interval*2} and size_estimate <= ${interval*3} THEN 1 END)`), `${interval*2} - ${interval*3}`],
+             [Sequelize.literal(`COUNT(CASE WHEN size_estimate > ${interval*3} and size_estimate <= ${interval*4} THEN 1 END)`), `${interval*3} - ${interval*4}`],
+             [Sequelize.literal(`COUNT(CASE WHEN size_estimate > ${interval*4} and size_estimate <= ${interval*5} THEN 1 END)`), `${interval*4} - ${interval*5}`],
+             [Sequelize.literal(`COUNT(CASE WHEN size_estimate > ${interval*5} THEN 1 END)`), `> ${interval*5}`]
+             
+            ]
+        
+    })
+}
+
+async function getCompaniesByAgeOfBusiness(data){
+    let max_size_estimate =  await model.companies.findOne({
+        attributes: [
+           
+            [Sequelize.literal('MAX(age_of_business)'), "max_size_estimate"]
+        ],
+        ...data
+    })
+    max_size_estimate = max_size_estimate? max_size_estimate.dataValues.max_size_estimate: 0
+
+    let interval = max_size_estimate / 6
+    
+    interval = floor(interval)
+
+    return await model.companies.findAll({
+        attributes: [
+             [Sequelize.literal(`COUNT(CASE WHEN age_of_business = 0 THEN 1 END)`), "0"],
+             [Sequelize.literal(`COUNT(CASE WHEN age_of_business > 0 and age_of_business <=${interval} THEN 1 END)`), `0 - ${interval}`],
+             [Sequelize.literal(`COUNT(CASE WHEN age_of_business > ${interval} and age_of_business <=${interval*2} THEN 1 END)`), `${interval} - ${interval*2}`],
+             [Sequelize.literal(`COUNT(CASE WHEN age_of_business > ${interval*2} and age_of_business <= ${interval*3} THEN 1 END)`), `${interval*2} - ${interval*3}`],
+             [Sequelize.literal(`COUNT(CASE WHEN age_of_business > ${interval*3} and age_of_business <= ${interval*4} THEN 1 END)`), `${interval*3} - ${interval*4}`],
+             [Sequelize.literal(`COUNT(CASE WHEN age_of_business > ${interval*4} and age_of_business <= ${interval*5} THEN 1 END)`), `${interval*4} - ${interval*5}`],
+             [Sequelize.literal(`COUNT(CASE WHEN age_of_business > ${interval*5} THEN 1 END)`), `> ${interval*5}`]
+             
+            ]
+        
+    })
+}
+
+async function getCompaniesByTurnover(data){
+    let maximum =  await model.companies.findOne({
+        attributes: [
+           [Sequelize.literal('MAX(turnover_estimate)'), "maximum"]
+        ],
+        ...data
+    })
+    let minimum =  await model.companies.findOne({
+        attributes: [
+           [Sequelize.literal('MIN(turnover_estimate)'), "minimum"]
+        ],
+        ...data
+    })
+    maximum = maximum? maximum.dataValues.maximum: 0
+    minimum = minimum? minimum.dataValues.minimum: 0
+
+    let diffrence = maximum - minimum
+    let interval = diffrence / 7
+    
+    interval = floor(interval)
+
+    return await model.companies.findAll({
+        attributes: [
+             [Sequelize.literal(`COUNT(CASE WHEN turnover_estimate = ${ minimum} and turnover_estimate <= ${ minimum + interval} THEN 1 END)`), `${ minimum} - ${ minimum + interval} `],
+             [Sequelize.literal(`COUNT(CASE WHEN turnover_estimate > ${minimum + interval} and turnover_estimate <=${minimum + interval} THEN 1 END)`), `${ minimum + interval} - ${ minimum + interval*2} `],
+             [Sequelize.literal(`COUNT(CASE WHEN turnover_estimate > ${minimum + interval*2} and turnover_estimate <=${minimum + interval*2} THEN 1 END)`), `${ minimum + interval*2} - ${ minimum + interval*3} `],
+             [Sequelize.literal(`COUNT(CASE WHEN turnover_estimate > ${minimum + interval*3} and turnover_estimate <= ${minimum + interval*3} THEN 1 END)`), `${ minimum + interval*3} - ${ minimum + interval*4} `],
+             [Sequelize.literal(`COUNT(CASE WHEN turnover_estimate > ${minimum + interval*4} and turnover_estimate <= ${minimum + interval*4} THEN 1 END)`), `${ minimum + interval*4} - ${ minimum + interval*5} `],
+             [Sequelize.literal(`COUNT(CASE WHEN turnover_estimate > ${minimum + interval*5} and turnover_estimate <= ${minimum + interval*5} THEN 1 END)`), `${ minimum + interval*5} - ${ minimum + interval*6} `],
+             [Sequelize.literal(`COUNT(CASE WHEN turnover_estimate > ${minimum + interval*6} THEN 1 END)`), `> ${ minimum + interval*6}`]
+             
+            ]
+        
+    })
+}
+
 module.exports = {
     getCompanies,
-    getCompaniesOfficial
+    getCompaniesOfficial,
+    getCompaniesByEmployeeSize,
+    getCompaniesByAgeOfBusiness,
+    getCompaniesByTurnover
 }
